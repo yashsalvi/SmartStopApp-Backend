cb9251cd1d189cd4145dcdc656580031baea626a
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-03-31 19:45:44 +0530

    Update image.controller.js

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -3,7 +3,7 @@ const models = require('../models');
 function upload(req, res){
   const id = req.params.id;
   if(req.method == 'POST'){
-    if(req.files.drivingLicense || req.files.handgunLicense || req.files.insurance || req.files.profileimage)
+    if(req.files.drivingLicense || req.files.handgunLicense || req.files.insurance || req.files.profileImage)
     {
       let handgun = true;
       if(req.files.handgunLicense)
@@ -12,7 +12,7 @@ function upload(req, res){
         handgun = false;
 
       const drivingLicense = 'http://localhost:8086/uploads/' + req.files.drivingLicense[0].filename;
-      const profileimage = 'http://localhost:8086/uploads/' + req.files.profileimage[0].filename;
+      const profileImage = 'http://localhost:8086/uploads/' + req.files.profileImage[0].filename;
       const insurance = 'http://localhost:8086/uploads/' + req.files.insurance[0].filename;
       let handgunLicense = null;
       if(handgun)
@@ -34,7 +34,7 @@ function upload(req, res){
                   });
           models.User.findOne({ where: {id : req.params.id}})
           .then(function (record) {
-              return record.update({handgunlicense:(handgun?handgunLicense:"NULL"),drivinglicense:drivingLicense,insurance:insurance});
+              return record.update({handgunLicense:(handgun?handgunLicense:"NULL"),drivingLicense:drivingLicense,insurance:insurance});
             });
           }
         else{
@@ -84,76 +84,10 @@ function upload(req, res){
 }
 
 
-// function updatedocs(req,res){
-//   const id = req.params.id;
-//   if(req.method == 'POST'){
-//     if(req.files.drivingLicense || req.files.handgunLicense || req.files.insurance)
-//     {
-//       let handgun = true;
-//       if(req.files.handgunLicense)
-//         handgun = true;
-//       else
-//         handgun = false;
-
-//       const drivingLicense = 'http://localhost:8086/uploads/' + req.files.drivingLicense[0].filename;
-//       const insurance = 'http://localhost:8086/uploads/' + req.files.insurance[0].filename;
-//       let handgunLicense = null;
-//       if(handgun)
-//       {
-//         handgunLicense = 'http://localhost:8086/uploads/' + req.files.handgunLicense[0].filename;
-//       }
-//       models.User.findByPk(id)
-//       .then(result => {
-//         if(result){
-//           if(result.isPolice == false){
-//               res.status(200).json({
-//                         success:
-//                           {        
-//                             message:"Image uploaded and stored successfully!",
-//                             handgunLicense,                
-//                             drivingLicense,                
-//                             insurance
-//                           }
-//                   });
-//           models.User.findOne({ where: {id : req.params.id}})
-//           .then(function (record) {
-//               return record.update({handgunlicense:(handgun?handgunLicense:"NULL"),drivinglicense:drivingLicense,insurance:insurance});
-//             });
-//           }
-//         else{
-//             res.status(500).json({
-//               message: "Invalid credentials"
-//             })
-//           }
-//         }
-//       })
-//       .catch(error => {
-//         res.status(500).json({
-//             message: "There is no such id",
-//         });
-//       });
-//     }
-//     else{
-//       res.status(500).json({
-//         message: "Please insert images"
-//       })
-//     }
-//   }
-//   else{
-//     res.status(500).json({
-//       message: "Error in uploading images"
-//     })
-//   }
-
-
-
-// }
-
 
 
 module.exports = {
     upload:upload,
-    // updatedocs:updatedocs
     
 }
 
