9abeac94df167f15e518c8599710e022e7e0a85c
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-03 19:00:18 +0530

    Updated the Upload and User path

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-
 const userRoute = require('./routes/user');
 const stopLogRoute = require('./routes/stopLog');
 
 const imageRoute = require('./routes/images');
 
 const app = express();
-
 app.use(bodyParser.json());
 
 app.use('/uploads', express.static('uploads'));
-
-
 app.use("/user", userRoute);
 app.use("/stopLog", stopLogRoute);
-
 app.use("/images", imageRoute);
+
 
 module.exports = app;
