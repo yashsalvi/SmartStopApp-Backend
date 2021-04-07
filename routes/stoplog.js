3003ad18d860b63637f3c0b24b6ada1e36cd73e1
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-07 19:41:28 +0530

    Update stoplog.js

--- routes/stoplog.js
+++ routes/stoplog.js
@@ -2,7 +2,6 @@ const express = require('express');
 const stoplogController = require('../controllers/stoplog.controller');
 const userController = require('../controllers/user.controller');
 const router = express.Router();
-
 router.post('/stops', stoplogController.stops);
 
 module.exports = router;
