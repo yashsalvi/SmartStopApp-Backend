4f839013a6367d97ebc0eb7b33bc003f7389263a
yashsalvi <47267252+yashsalvi@users.noreply.github.com>
2021-04-07 19:39:43 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -13,7 +13,6 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
 fs
   .readdirSync(__dirname)
   .filter(file => {
